2bd65b13d91175c5e9e99666ca9b8a09ac2694dc
Adam Lusch <adam.lusch@autodesk.com>
2025-03-20 19:09:59 -0500

    Make test more specific

--- test/gtx/gtx_compatibility.cpp
+++ test/gtx/gtx_compatibility.cpp
@@ -30,19 +30,19 @@ int main()
 	Error += glm::isfinite(-1.0f/Zero_f) ? 1 : 0;
 	Error += glm::isfinite(-1.0/Zero_d) ? 1 : 0;
 
-	Error += glm::all(glm::isfinite(glm::vec4(0.0f/Zero_f))) ? 1 : 0;
-	Error += glm::all(glm::isfinite(glm::dvec4(0.0/Zero_d))) ? 1 : 0;
-	Error += glm::all(glm::isfinite(glm::vec4(1.0f/Zero_f))) ? 1 : 0;
-	Error += glm::all(glm::isfinite(glm::dvec4(1.0/Zero_d))) ? 1 : 0;
-	Error += glm::all(glm::isfinite(glm::vec4(-1.0f/Zero_f))) ? 1 : 0;
-	Error += glm::all(glm::isfinite(glm::dvec4(-1.0/Zero_d))) ? 1 : 0;
+	Error += glm::any(glm::isfinite(glm::vec4(0.0f/Zero_f))) ? 1 : 0;
+	Error += glm::any(glm::isfinite(glm::dvec4(0.0/Zero_d))) ? 1 : 0;
+	Error += glm::any(glm::isfinite(glm::vec4(1.0f/Zero_f))) ? 1 : 0;
+	Error += glm::any(glm::isfinite(glm::dvec4(1.0/Zero_d))) ? 1 : 0;
+	Error += glm::any(glm::isfinite(glm::vec4(-1.0f/Zero_f))) ? 1 : 0;
+	Error += glm::any(glm::isfinite(glm::dvec4(-1.0/Zero_d))) ? 1 : 0;
 
-	Error += glm::all(glm::isfinite(glm::quat(0.0f/Zero_f, 0.0f/Zero_f, 0.0f/Zero_f, 0.0f/Zero_f))) ? 1 : 0;
-	Error += glm::all(glm::isfinite(glm::dquat(0.0/Zero_d, 0.0/Zero_d, 0.0/Zero_d, 0.0/Zero_d))) ? 1 : 0;
-	Error += glm::all(glm::isfinite(glm::quat(1.0f/Zero_f, 1.0f/Zero_f, 1.0f/Zero_f, 1.0f/Zero_f))) ? 1 : 0;
-	Error += glm::all(glm::isfinite(glm::dquat(1.0/Zero_d, 1.0/Zero_d, 1.0/Zero_d, 1.0/Zero_d))) ? 1 : 0;
-	Error += glm::all(glm::isfinite(glm::quat(-1.0f/Zero_f, -1.0f/Zero_f, -1.0f/Zero_f, -1.0f/Zero_f))) ? 1 : 0;
-	Error += glm::all(glm::isfinite(glm::dquat(-1.0/Zero_d, -1.0/Zero_d, -1.0/Zero_d, -1.0/Zero_d))) ? 1 : 0;
+	Error += glm::any(glm::isfinite(glm::quat(0.0f/Zero_f, 0.0f/Zero_f, 0.0f/Zero_f, 0.0f/Zero_f))) ? 1 : 0;
+	Error += glm::any(glm::isfinite(glm::dquat(0.0/Zero_d, 0.0/Zero_d, 0.0/Zero_d, 0.0/Zero_d))) ? 1 : 0;
+	Error += glm::any(glm::isfinite(glm::quat(1.0f/Zero_f, 1.0f/Zero_f, 1.0f/Zero_f, 1.0f/Zero_f))) ? 1 : 0;
+	Error += glm::any(glm::isfinite(glm::dquat(1.0/Zero_d, 1.0/Zero_d, 1.0/Zero_d, 1.0/Zero_d))) ? 1 : 0;
+	Error += glm::any(glm::isfinite(glm::quat(-1.0f/Zero_f, -1.0f/Zero_f, -1.0f/Zero_f, -1.0f/Zero_f))) ? 1 : 0;
+	Error += glm::any(glm::isfinite(glm::dquat(-1.0/Zero_d, -1.0/Zero_d, -1.0/Zero_d, -1.0/Zero_d))) ? 1 : 0;
 
 	return Error;
 }
