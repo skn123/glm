3aed772ae927b7d97ff6957853b589b7317dab65
Adam Lusch <adam.lusch@autodesk.com>
2025-03-20 19:02:53 -0500

    isfinite support for quaternions

--- test/gtx/gtx_compatibility.cpp
+++ test/gtx/gtx_compatibility.cpp
@@ -5,6 +5,9 @@ int main()
 {
 	int Error(0);
 
+	float Zero_f = 0.0;
+	double Zero_d = 0.0;
+
 	Error += glm::isfinite(1.0f) ? 0 : 1;
 	Error += glm::isfinite(1.0) ? 0 : 1;
 	Error += glm::isfinite(-1.0f) ? 0 : 1;
@@ -14,6 +17,32 @@ int main()
 	Error += glm::all(glm::isfinite(glm::dvec4(1.0))) ? 0 : 1;
 	Error += glm::all(glm::isfinite(glm::vec4(-1.0f))) ? 0 : 1;
 	Error += glm::all(glm::isfinite(glm::dvec4(-1.0))) ? 0 : 1;
+
+	Error += glm::all(glm::isfinite(glm::quat(1.0f, 1.0f, 1.0f, 1.0f))) ? 0 : 1;
+	Error += glm::all(glm::isfinite(glm::dquat(1.0, 1.0, 1.0, 1.0))) ? 0 : 1;
+	Error += glm::all(glm::isfinite(glm::quat(-1.0f, -1.0f, -1.0f, -1.0f))) ? 0 : 1;
+	Error += glm::all(glm::isfinite(glm::dquat(-1.0, -1.0, -1.0, -1.0))) ? 0 : 1;
+
+	Error += glm::isfinite(0.0f/Zero_f) ? 1 : 0;
+	Error += glm::isfinite(0.0/Zero_d) ? 1 : 0;
+	Error += glm::isfinite(1.0f/Zero_f) ? 1 : 0;
+	Error += glm::isfinite(1.0/Zero_d) ? 1 : 0;
+	Error += glm::isfinite(-1.0f/Zero_f) ? 1 : 0;
+	Error += glm::isfinite(-1.0/Zero_d) ? 1 : 0;
+
+	Error += glm::all(glm::isfinite(glm::vec4(0.0f/Zero_f))) ? 1 : 0;
+	Error += glm::all(glm::isfinite(glm::dvec4(0.0/Zero_d))) ? 1 : 0;
+	Error += glm::all(glm::isfinite(glm::vec4(1.0f/Zero_f))) ? 1 : 0;
+	Error += glm::all(glm::isfinite(glm::dvec4(1.0/Zero_d))) ? 1 : 0;
+	Error += glm::all(glm::isfinite(glm::vec4(-1.0f/Zero_f))) ? 1 : 0;
+	Error += glm::all(glm::isfinite(glm::dvec4(-1.0/Zero_d))) ? 1 : 0;
+
+	Error += glm::all(glm::isfinite(glm::quat(0.0f/Zero_f, 0.0f/Zero_f, 0.0f/Zero_f, 0.0f/Zero_f))) ? 1 : 0;
+	Error += glm::all(glm::isfinite(glm::dquat(0.0/Zero_d, 0.0/Zero_d, 0.0/Zero_d, 0.0/Zero_d))) ? 1 : 0;
+	Error += glm::all(glm::isfinite(glm::quat(1.0f/Zero_f, 1.0f/Zero_f, 1.0f/Zero_f, 1.0f/Zero_f))) ? 1 : 0;
+	Error += glm::all(glm::isfinite(glm::dquat(1.0/Zero_d, 1.0/Zero_d, 1.0/Zero_d, 1.0/Zero_d))) ? 1 : 0;
+	Error += glm::all(glm::isfinite(glm::quat(-1.0f/Zero_f, -1.0f/Zero_f, -1.0f/Zero_f, -1.0f/Zero_f))) ? 1 : 0;
+	Error += glm::all(glm::isfinite(glm::dquat(-1.0/Zero_d, -1.0/Zero_d, -1.0/Zero_d, -1.0/Zero_d))) ? 1 : 0;
 
 	return Error;
 }
